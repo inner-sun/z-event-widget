05d08e8c8ad5a5c4e9781ff847ba64743c297422
InnerSun <167511709+inner-sun@users.noreply.github.com>
2024-09-04 23:55:15 +0200

    GoalsList: added Total Raised amount, can be controlled with up/down arrows, updates the reached prop of each GoalEntry

--- src/components/goals-list/goals-list.tsx
+++ src/components/goals-list/goals-list.tsx
@@ -1,19 +1,53 @@
-import { Component, For } from 'solid-js'
+import { Component, createSignal, For, onMount } from 'solid-js'
 import { donationGoals } from '~/assets/data/donation-goals'
 import GoalEntry from '~/components/goal-entry/goal-entry'
 import styles from '~/components/goals-list/goals-list.module.scss'
 
 const GoalsList: Component = () => {
+  const [raisedAmount, setRaisedAmount] = createSignal(4999)
+
+  onMount(() => {
+    window.addEventListener('keyup', (event) => {
+      const currentAmount = raisedAmount()
+      let newGoal = null
+      if(event.key === 'ArrowDown'){
+        newGoal = donationGoals.find(entry => entry.amount > currentAmount)
+      }
+      if(event.key === 'ArrowUp'){
+        newGoal = donationGoals.slice().reverse().find(entry => entry.amount < currentAmount)
+      }
+      if(newGoal){
+        setRaisedAmount(newGoal.amount)
+      }
+    })
+  })
+
   return (
-    <div class={styles.goalsList}>
-      <For each={donationGoals}>
-        {entry => (
-          <GoalEntry
-            amount={entry.amount}
-            label={entry.label}
-          />
-        )}
-      </For>
+    <div class={styles.container}>
+      <div class={styles.raisedAmount}>
+        <div class={styles.label}>
+          Cagnotte
+        </div>
+        <div class={styles.value}>
+          {raisedAmount()} <span class={styles.currency}>€</span>
+        </div>
+      </div>
+
+      <div class={styles.progressBar}>
+        <div class={styles.progress} />
+      </div>
+
+      <main class={styles.goalsList}>
+        <For each={donationGoals}>
+          {entry => (
+            <GoalEntry
+              amount={entry.amount}
+              label={entry.label}
+              reached={raisedAmount() >= entry.amount}
+            />
+          )}
+        </For>
+      </main>
     </div>
   )
 }
